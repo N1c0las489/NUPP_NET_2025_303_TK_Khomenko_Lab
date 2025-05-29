c1cc021aaea3514f92d8d6f999dcb974f15fbbd8
N1c0las489 <129153872+N1c0las489@users.noreply.github.com>
2025-05-29 16:46:31 +0300

    Add files via upload

---
+++ University2.REST/University2.REST/Controllers/EnrollmentsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University2.REST.Models;
+using University2.REST.Interfaces;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EnrollmentsController : ControllerBase
+{
+    private readonly ICrudServiceAsync<EnrollmentModel> _enrollmentService;
+
+    public EnrollmentsController(ICrudServiceAsync<EnrollmentModel> enrollmentService)
+    {
+        _enrollmentService = enrollmentService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<EnrollmentModel>> PostEnrollment(
+        [FromBody] EnrollmentCreateModel dto)
+    {
+        var enrollment = new EnrollmentModel
+        {
+            StudentId = dto.StudentId,
+            CourseId = dto.CourseId,
+            Grade = dto.Grade
+        };
+
+        await _enrollmentService.CreateAsync(enrollment);
+        return CreatedAtAction(nameof(GetEnrollment), new { id = enrollment.Id }, enrollment);
+    }
+
+    private object GetEnrollment()
+    {
+        throw new NotImplementedException();
+    }
+}
